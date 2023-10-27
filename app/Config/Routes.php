fdc6d626615964cf853ff663aa6c744dc1023f49
aprilianggun <aprilia.anggun21@students.unila.ac.id>
2023-10-27 11:32:22 +0700

    pesan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\UserController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
@@ -7,4 +8,9 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 #$routes->get('/profile','Home::profile');
-$routes->get('/profile/(:any)/(:any)/(:any)', 'Home::profile/$1/$2/$3');
+$routes->get('/profile/(:any)/(:any)/(:any)', 'UserController::profile/$1/$2/$3');
+
+# create
+$routes->get('/user/create', 'UserController::create');
+#$routes->post('/user/store', 'UserController::store');
+$routes->post('/user/store', 'UserController::store');
